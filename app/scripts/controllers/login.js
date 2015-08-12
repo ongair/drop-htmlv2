0133508fc129285997bc0f9b9cdced120eab739d
Evans Mwendwa <evansmwendwa@live.com>
2015-08-12 03:20:40 +0300

    added support for authentication

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,5 +1,42 @@
-bbcControllers.controller('LoginCtrl', ['$scope', '$http',
-    function ($scope, $http) {
+bbcControllers.controller('LoginCtrl', [
+    '$scope',
+    '$http',
+    'AuthService',
+
+    function ($scope, $http, AuthService) {
         $scope.controllerName = 'login';
+
+        AuthService.initialize();
+
+        $scope.authenticate = function(provider) {
+            AuthService.authenticate(provider).then(function() {
+                if (AuthService.isReady()) {
+                    $scope.connectedUser = true;
+                    // login successful create a session and connect it to the
+                    // backend to allow api requests
+
+                } else {
+                    // show an error message
+                }
+            });
+        };
+
+        //sign out clears the OAuth cache, the user will have to reauthenticate when returning
+        $scope.signOut = function() {
+            AuthService.clearCache();
+            $scope.connectedUser = false
+            console.log('loged out');
+            // todo:
+            // end the backend session
+            // somewhere redirect the user to login
+        }
+
+        if(AuthService.isReady()) {
+            $scope.connectedUser = true;
+
+            // fetch the user session from the backend
+            // redirect the user using the new session to articles
+        }
+
     }
 ]);
