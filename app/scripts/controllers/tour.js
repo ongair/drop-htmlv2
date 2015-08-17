6f968f186b662f3fda2fccc320cad80b2ddd7dff
Evans Mwendwa <evansmwendwa@live.com>
2015-08-17 14:58:37 +0300

    automatically redirect logged in users to articles page

--- app/scripts/controllers/tour.js
+++ app/scripts/controllers/tour.js
@@ -1,5 +1,17 @@
-appControllers.controller('TourCtrl', ['$scope',
-    function ($scope) {
+appControllers.controller('TourCtrl', [
+    '$scope',
+    'AuthService',
+    '$state',
+
+    function ($scope, AuthService, $state) {
         $scope.controllerName = 'tour';
+
+        if(AuthService.isReady()) {
+            $scope.connectedUser = true;
+            $state.transitionTo('articles');
+            // fetch the user session from the backend
+            // redirect the user using the new session to articles
+        }
     }
+
 ]);
