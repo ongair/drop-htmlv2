6774878aa9fde0484cebbe9adae0d8b35f7fd42f
Evans Mwendwa <evansmwendwa@live.com>
2015-08-17 13:32:24 +0300

    add padding space to articles landing page

--- app/scripts/controllers/article.js
+++ app/scripts/controllers/article.js
@@ -6,6 +6,7 @@ appControllers.controller('ArticleCtrl', ['$scope', '$http','$stateParams',
 
         $http.get('http://drop.ongair.im/api/articles/'+$stateParams.articleId+'.json')
         .then(function(response){
+            response.data.image_url = 'http://ichef-1.bbci.co.uk/news/976/cpsprodpb/11F0A/production/_84928437_028590051-1.jpg';
             $scope.article =  response.data;
         }, function(response) {
             // log error
