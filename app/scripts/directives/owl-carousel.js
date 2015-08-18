b60c8c67a419ff57c6d94a35ffb6ecfe02e2328d
Evans Mwendwa <evansmwendwa@live.com>
2015-08-18 03:01:56 +0300

    removed owl carousel

--- app/scripts/directives/owl-carousel.js
+++
@@ -1,9 +0,0 @@
-appDirectives.directive('wrapOwlcarousel', function () {
-    return {
-        restrict: 'E',
-        link: function (scope, element, attrs) {
-            var options = scope.$eval($(element).attr('data-options'));
-            $(element).owlCarousel(options);
-        }
-    };
-});
