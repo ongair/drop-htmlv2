52ddbdd0804b5407ff5c6598ccb7f6f6cf9fbec3
Evans Mwendwa <evansmwendwa@live.com>
2015-08-18 03:02:15 +0300

    added params for bootstrap carousel

--- app/scripts/controllers/articles.js
+++ app/scripts/controllers/articles.js
@@ -2,6 +2,8 @@ appControllers.controller('ArticlesCtrl', ['$scope', '$http',
     function ($scope, $http) {
 
         $scope.controllerName = 'articles';
+        $scope.myInterval = 0;
+        $scope.noWrapSlides = false;
 
         $http.get('http://drop.ongair.im/api/articles.json')
         .then(function(response){
