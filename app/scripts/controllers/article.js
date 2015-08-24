1f082f1e076c0c82fb1751cc1279fa5d3bd1aab5
Evans Mwendwa <evansmwendwa@live.com>
2015-08-24 13:26:49 +0300

    retrieve search term from stateparams

--- app/scripts/controllers/article.js
+++ app/scripts/controllers/article.js
@@ -2,6 +2,7 @@ appControllers.controller('ArticleCtrl', ['$scope', '$http','$stateParams',
     function ($scope, $http, $stateParams) {
 
         $scope.controllerName = 'article';
+        $scope.term = $stateParams.term;
 
         $http.get('http://drop.ongair.im/api/articles/'+$stateParams.articleid+'.json')
         .then(function(response){
