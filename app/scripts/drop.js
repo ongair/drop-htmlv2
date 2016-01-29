94742a886bc017813036797153fa5a0d4483c051
Trevor Kimenye <kimenye@gmail.com>
2016-01-29 07:59:16 +0300

    Added get drop-id

--- app/scripts/drop.js
+++ app/scripts/drop.js
@@ -4,3 +4,12 @@ var Drop = Drop || {};
 Drop.getBaseUrl = function() {
   return document.location.hostname;
 }
+
+Drop.getDropId = function() {
+  var regex = /bbc-drop-uk/;
+  var id = "bbc-drop";
+  if (regex.exec(Drop.getBaseUrl()) != null) {
+    id = "bbc-drop-uk";
+  }
+  return id;
+}
